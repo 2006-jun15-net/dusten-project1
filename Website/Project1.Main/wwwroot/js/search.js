ac10536d068739b30f28eace7f3df6faac85977b
Dusten Knull <dakatk97@gmail.com>
2020-07-12 13:07:36 -0500

    Added search function to dropdown lists

---
+++ Website/Project1.Main/wwwroot/js/search.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+
+    $('#search').keyup(function (event) {
+
+        event.preventDefault();
+
+        let inputStr = $(this).attr('value').toLowerCase();
+
+        $('#search-menu a').each(function (index, value) {
+
+            console.log(value);
+
+            let textValue = $(this).attr('value').toLowerCase();
+
+            if (textValue.indexOf(inputStr) > -1) {
+                $(this).css('display', 'block');
+            }
+
+            else {
+                $(this).css('display', 'none');
+            }
+        });
+    });
+
+});
